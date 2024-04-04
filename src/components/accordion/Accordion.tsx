ffa17a3b4aca67eac43761cc4aba4b2a130212de
Nicolette | Nikki <info@nicolette.codes>
2024-04-04 09:23:31 +0200

    finish mobile version

--- src/components/accordion/Accordion.tsx
+++ src/components/accordion/Accordion.tsx
@@ -1,59 +1,75 @@
 import AccordionItem from "./AccordionItem";
 import type Accordion from "../../types/Accordion";
+import { useState } from "react";
+
+const accordionArray: Accordion[] = [
+	{
+		id: 1,
+		question: "What is Frontend Mentor, and how will it help me?",
+		answer: `Frontend Mentor offers realistic coding challenges to help
+			developers improve their frontend coding skills with projects in
+			HTML, CSS, and JavaScript. It's suitable for all levels and ideal
+			for portfolio building.`,
+		isOpen: true,
+	},
+
+	{
+		id: 2,
+		question: "Is Frontend Mentor free?",
+		answer: `Yes, Frontend Mentor offers both free and
+			premium coding challenges, with the free option providing access to
+			a range of projects suitable for all skill levels.`,
+		isOpen: false,
+	},
+	{
+		id: 3,
+		question: "Can I use Frontend Mentor projects in my portfolio?",
+		answer: `Yes, you can use projects completed on Frontend Mentor in your
+			portfolio. It's an excellent way to showcase your skills to
+			potential employers!`,
+		isOpen: false,
+	},
+	{
+		id: 4,
+		question:
+			"How can I get help if I'm stuck on a Frontend Mentor challenge?",
+		answer: `The best place to get help is inside Frontend Mentor's Discord
+			community. There's a help channel where you can ask questions and
+			seek support from other community members.`,
+		isOpen: false,
+	},
+];
 
 export default function FaqAccordion() {
-	const accordionArray: Accordion[] = [
-		{
-			id: 1,
-			question: "What is Frontend Mentor, and how will it help me?",
-			answer: `Frontend Mentor offers realistic coding challenges to help
-                developers improve their frontend coding skills with projects in
-                HTML, CSS, and JavaScript. It's suitable for all levels and ideal
-                for portfolio building.`,
-			isOpen: false,
-		},
-		{
-			id: 2,
-			question: "Is Frontend Mentor free?",
-			answer: `Yes, Frontend Mentor offers both free and
-                premium coding challenges, with the free option providing access to
-                a range of projects suitable for all skill levels.`,
-			isOpen: false,
-		},
-		{
-			id: 3,
-			question: "Can I use Frontend Mentor projects in my portfolio?",
-			answer: `Yes, you can use projects completed on Frontend Mentor in your
-                portfolio. It's an excellent way to showcase your skills to
-                potential employers!`,
-			isOpen: false,
-		},
-		{
-			id: 4,
-			question:
-				"How can I get help if I'm stuck on a Frontend Mentor challenge?",
-			answer: `The best place to get help is inside Frontend Mentor's Discord
-                community. There's a help channel where you can ask questions and
-                seek support from other community members.`,
-			isOpen: false,
-		},
-	];
+	const [faqs, setFaqs] = useState(accordionArray);
+
+	function toggleVisibility(id: number) {
+		setFaqs((faqs) =>
+			faqs.map((faq) =>
+				faq.id === id ? { ...faq, isOpen: !faq.isOpen } : faq,
+			),
+		);
+	}
 
 	return (
-		<section className="faq-card">
-			<div className="faq-card__header">
+		<section className="accordion">
+			<div className="accordion__header">
 				<img
 					src="../../../src/assets/images/icon-star.svg"
 					alt="Purple 4 sided star icon."
-					className="icon-star"
+					className="icon"
 				/>
-				<h1 className="faq-card__heading">FAQs</h1>
+				<h1 className="accordion__heading">FAQs</h1>
 			</div>
-			<div className="faq-card__accordion">
-				{accordionArray.map((faqItem) => {
-					return <AccordionItem {...faqItem} key={faqItem.id} />;
-				})}
-			</div>
+			{faqs.map((faqItem) => {
+				return (
+					<AccordionItem
+						onVisibilityChange={toggleVisibility}
+						items={faqItem}
+						key={faqItem.id}
+					/>
+				);
+			})}
 		</section>
 	);
 }
--- src/components/accordion/AccordionItem.tsx
+++ src/components/accordion/AccordionItem.tsx
@@ -1,11 +1,52 @@
 import Accordion from "../../types/Accordion";
 
-export default function AccordionItem(props: Accordion) {
+interface AccordionItemProps {
+	items: Accordion;
+	onVisibilityChange(id: number): void;
+}
+
+export default function AccordionItem({
+	items,
+	onVisibilityChange,
+}: AccordionItemProps) {
 	return (
-		<div className="accordion__item">
-			<div className="faq__question">{props.question}</div>
-			<div className="faq__answer">{props.answer}</div>
-			<div className="faq__seperator"></div>
+		<div className="accordion__panel">
+			<h2 id={`panel-${items.id}-heading`}>
+				<button
+					className="accordion__title accordion--trigger"
+					aria-controls={`panel-${items.id}-content`}
+					aria-expanded={items.isOpen}
+					onClick={() => onVisibilityChange(items.id)}
+				>
+					<div id="panel-1-title">{items.question}</div>
+					{items.isOpen ? (
+						<img
+							aria-hidden="true"
+							src="../../../src/assets/images/icon-minus.svg"
+							alt="Minus icon to close the answer."
+							className="icon"
+						/>
+					) : (
+						<img
+							aria-hidden="true"
+							src="../../../src/assets/images/icon-plus.svg"
+							alt="Plus icon to open the answer."
+							className="icon"
+						/>
+					)}
+				</button>
+			</h2>
+			<div
+				id={`panel-${items.id}-content`}
+				className="accordion__content"
+				aria-labelledby={`panel-${items.id}-heading`}
+				aria-hidden={items.isOpen}
+				role="region"
+			>
+				<div>
+					<p>{items.answer}</p>
+				</div>
+			</div>
 		</div>
 	);
 }
